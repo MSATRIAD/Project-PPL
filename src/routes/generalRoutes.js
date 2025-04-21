ebdac1c97a4524777ddddf3cb41c8ab181d4c8e2
Rumi Rifai <rumirifai96@gmail.com>
2025-04-21 09:45:32 +0700

    Feat: Add history controller and routes

--- src/routes/generalRoutes.js
+++ src/routes/generalRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const articleController = require('../controllers/articleController');
 const recycleController = require('../controllers/recycleController');
+const historyController = require('../controllers/historyController')
 
 router.get('/articles', articleController.getArticles);
 router.post('/recycling-info', recycleController.getRecyclingInfo);
+router.get('/history', historyController.getHistory);
+router.get('/historyById', historyController.getHistoryById);
 
 module.exports = router;
