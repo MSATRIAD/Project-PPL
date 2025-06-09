dfbfada94fbd0da4bf5747e991b8851f2103ca93
Rumi Rifai <rumirifai96@gmail.com>
2025-06-09 21:27:00 +0700

    Fixing

--- src/middlewares/verifyJWT.js
+++ src/middlewares/verifyJWT.js
@@ -4,9 +4,7 @@ function verifyJWT(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (!token) return res
-    .status(401)
-    .json({ message: "Unauthorized: Akses ditolak atau token tidak valid." });
+  if (!token) return res.status(401).json({ message: "Unauthorized: Akses ditolak atau token tidak valid." });
 
   jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403); 
