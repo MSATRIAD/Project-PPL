e508a78b0e626661682e1b3e140282c510ab33d2
Rumi Rifai <rumirifai96@gmail.com>
2025-06-12 14:46:43 +0700

    Change response

--- src/controllers/historyController.js
+++ src/controllers/historyController.js
@@ -25,23 +25,45 @@ exports.getHistoryById = async (req, res) => {
   const history_id = req.params.history_id;
 
   if (!user_id || !history_id) {
-    return res.status(401).json({ message: "Id undefined" });
+    return res.status(401).json({ message: "Id tidak lengkap." });
   }
 
   try {
-    const historyResult = await pool.query(
-      "SELECT * FROM result_history WHERE user_id = $1 AND history_id = $2",
-      [user_id, history_id]
-    );
+    const queryText = `
+      SELECT
+        h.history_id,
+        h.image_url,
+        h.prediction_result,
+        h.created_at,
+        h.user_id,
+        h.recycle_id,
+        r.material_type,
+        r.can_be_recycled,
+        r.recycle_process,
+        r.possible_products
+      FROM
+        result_history h
+      JOIN
+        recycle_info r ON h.recycle_id = r.recycle_id
+      WHERE
+        h.user_id = $1 AND h.history_id = $2;
+    `;
+
+    const historyResult = await pool.query(queryText, [user_id, history_id]);
 
     if (historyResult.rows.length === 0) {
-      return res.status(404).json({message: "History not found"});
+      return res
+        .status(404)
+        .json({
+          message:
+            "History not found or you do not have permission to view it.",
+        });
     }
 
-    res.status(200).json(historyResult.rows[0]); // atau .rows jika ingin array
+    res.status(200).json(historyResult.rows[0]);
   } catch (err) {
-    console.error(err);
-    res.status(500).json({message: "Server error"});
+    console.error("Error fetching history detail:", err);
+    res.status(500).json({ message: "Server error" });
   }
 };
 
@@ -56,7 +78,6 @@ exports.uploadPredictionResult = async (req, res) => {
         .json({ message: "prediction_result wajib diisi." });
     }
 
-    // Ambil recycle_id berdasarkan prediction_result
     const recyclingInfoQuery = await pool.query(
       `SELECT * FROM recycle_info WHERE material_type = $1`,
       [prediction_result]
@@ -72,7 +93,6 @@ exports.uploadPredictionResult = async (req, res) => {
 
     const info = recyclingInfoQuery.rows[0];
 
-    // Simpan ke tabel history
     await pool.query(
       `INSERT INTO result_history (prediction_result, user_id, recycle_id)
        VALUES ($1, $2, $3)`,
