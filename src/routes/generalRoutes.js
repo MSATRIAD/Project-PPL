9c6072de2e472df725abedf5fab97756f244c3ef
Rumi Rifai <rumirifai96@gmail.com>
2025-06-12 16:55:00 +0700

    Fix: change profile routes

--- src/routes/generalRoutes.js
+++ src/routes/generalRoutes.js
@@ -11,13 +11,13 @@ router.get('/articles', articleController.getArticles);
 router.get('/history', historyController.getHistory);
 router.get('/history/:history_id', historyController.getHistoryById);
 router.post('/upload', historyController.uploadPredictionResult);
-router.get('/users/:user_id', profileController.getProfile);
+router.get('/profile', profileController.getProfile);
 router.get('/rewards', profileController.getAllRewards);
 router.post('/redeem', profileController.redeemReward);
-router.put('/users/edit/:user_id', profileController.editProfile);
+router.put('/profile/edit', profileController.editProfile);
 router.delete('/users/:user_id', profileController.deleteProfile);
 router.post(
-  '/users/upload-profile',
+  '/profile/upload',
   uploadProfile.single('profile_picture'), 
   profileController.uploadProfilePicture
 );
